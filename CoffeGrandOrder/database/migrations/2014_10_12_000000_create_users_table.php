90efeffb3c476ae85c9e96862420416064cd00ec
thanhlt <thanhlt43162@gmail.com>
2021-03-28 16:01:30 +0700

    Added migrations for creating database

--- CoffeGrandOrder/database/migrations/2014_10_12_000000_create_users_table.php
+++ CoffeGrandOrder/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+			$table->string('username');
             $table->string('name');
             $table->string('email')->unique();
+			$table->string('phone');
+			$table->string('address');
+			$table->integer('isverified');
+			$table->integer('permission');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
